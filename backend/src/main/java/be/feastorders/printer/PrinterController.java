eaa36af77d031a9f5416b13e5f6da301f5371887
alberto.rizzi <alberto.rizzi@carel.com>
2021-08-14 12:33:55 +0200

    #14 add thymeleaf and pdfbox libraries to work with pdf.

--- backend/src/main/java/be/feastorders/printer/PrinterController.java
+++ backend/src/main/java/be/feastorders/printer/PrinterController.java
@@ -1,12 +1,15 @@
 package be.feastorders.printer;
 
 import be.feastorders.printer.service.PrinterPOCService;
+import be.feastorders.printer.service.ReportService;
+import com.lowagie.text.DocumentException;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.print.PrintService;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,9 +23,12 @@ public class PrinterController {
     @Autowired
     private PrinterPOCService pocService;
 
+    @Autowired
+    private ReportService reportService;
+
     @ApiOperation("get printers")
     @ApiResponse(code = 200, message = "printers found", response = List.class)
-    @PostMapping("/list")
+    @GetMapping("/list")
     public List<String> getPrinters() {
         List<String> printers = pocService.getPrinterServices(null, null)
                 .stream().map(PrintService::getName).collect(Collectors.toList());
@@ -41,11 +47,37 @@ public class PrinterController {
         return pocService.print(printerName, allParams);
     }
 
+    @ApiOperation("print pdf to a real printer")
+    @ApiResponse(code = 200, message = "print pdf done", response = Boolean.class)
+    @GetMapping("/printPdf/{printerName}")
+//    @RequestParam(value = "copies", required = false)  Integer copies,
+//    @RequestParam(value = "mediaSize", required = false) String mediaSize,
+//    @RequestParam(value = "orientation", required = false, defaultValue = "portrait") String orientation,
+//    @RequestParam(value = "sides", required = false, defaultValue = "one-sided") String sides,
+//    @RequestParam(value = "color", required = false) Boolean color
+    public Boolean printPdf(@PathVariable String printerName, @RequestParam Map<String,String> allParams) {
+        return pocService.printPdf(printerName, allParams);
+    }
+
     @ApiOperation("print to a file")
     @ApiResponse(code = 200, message = "print done", response = Boolean.class)
     @GetMapping("/printToFile")
     public Boolean printToFile(@RequestParam Map<String,String> allParams) {
         return pocService.printToFile(allParams);
+    }
+
+    @ApiOperation("generate PDF using template engine")
+    @ApiResponse(code = 200, message = "pdf generated", response = Boolean.class)
+    @GetMapping("/pdf")
+    public Boolean createPdf() {
+        String html = reportService.parseThymeleafTemplate();
+        try {
+            reportService.generatePdfFromHtml(html);
+            return true;
+        } catch (DocumentException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
---
+++ backend/src/main/java/be/feastorders/printer/service/ReportService.java
@@ -0,0 +1,45 @@
+package be.feastorders.printer.service;
+
+import com.lowagie.text.DocumentException;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Service
+public class ReportService {
+
+    public String parseThymeleafTemplate() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("to", "Baeldung");
+
+        return templateEngine.process("templates/thymeleaf_template", context);
+    }
+
+    public void generatePdfFromHtml(String html) throws DocumentException, IOException {
+        String outputFolder = System.getProperty("user.home") + File.separator + "thymeleaf.pdf";
+        OutputStream outputStream = new FileOutputStream(outputFolder);
+
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(html);
+        renderer.layout();
+        renderer.createPDF(outputStream);
+
+        outputStream.close();
+    }
+
+}
