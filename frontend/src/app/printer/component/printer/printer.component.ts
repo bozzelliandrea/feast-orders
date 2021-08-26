59b5d9c066b409022ac8f4fdc2593d01efc7ca28
alberto.rizzi <alberto.rizzi@carel.com>
2021-08-26 14:50:01 +0200

    #34 add form for new printer cfg

--- frontend/src/app/printer/component/printer/printer.component.ts
+++ frontend/src/app/printer/component/printer/printer.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PrinterCfgService} from "../../service/printercfg.service";
 import {PrintersService} from "../../service/printers.service";
 import {first} from "rxjs/operators";
 import {PrinterCfg} from "../../interface/printercfg.interface";
 import {PrinterReportTemplateService} from "../../service/printer-report-template.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'printer',
@@ -15,15 +16,30 @@ export class PrinterComponent implements OnInit {
   printerCfgList: Array<PrinterCfg> = new Array<PrinterCfg>();
   printers: Array<string> = new Array<string>();
   reportTemplates: Array<string> = new Array<string>();
+  printerCfgForm: FormGroup;
 
   constructor(private _printerCfgService: PrinterCfgService,
               private _printersService: PrintersService,
-              private _printerReportTemplateService: PrinterReportTemplateService) { }
+              private _printerReportTemplateService: PrinterReportTemplateService,
+              private _formBuilder: FormBuilder
+  ) {
+    this._buildPrinterCfgForm();
+  }
 
   ngOnInit(): void {
     this._loadPrinterCfgList();
     this._loadPrinterList();
     this._loadPrinterReportTemplateList();
+  }
+
+  addNewPrinterCfg() {
+    const formValue = this.printerCfgForm.value;
+    const printerCfg: PrinterCfg = this._getPrinterCfgFromForm(formValue);
+    this._printerCfgService.create(printerCfg).pipe(first()).subscribe((res: any) => {
+      this._loadPrinterCfgList();
+    });
+
+    this.printerCfgForm.reset();
   }
 
   private _loadPrinterCfgList() {
@@ -44,4 +60,45 @@ export class PrinterComponent implements OnInit {
     });
   }
 
+  private _buildPrinterCfgForm() {
+    this.printerCfgForm = this._formBuilder.group({
+      id: [null],
+      version: [null],
+      creationTimestamp: [null],
+      updateTimestamp: [null],
+      creationUser: [null],
+      updateUser: [null],
+      name: [null, Validators.required],
+      description: [null],
+      printerName: [null, Validators.required],
+      reportTemplate: [null, Validators.required],
+      attrMediaSize: ['A4'],
+      attrOrientation: ['portrait'],
+      attrColor: [false],
+      attrSide: ['one-side'],
+      attrCopies: [1]
+    });
+  }
+
+  private _getPrinterCfgFromForm(formValue: any): PrinterCfg {
+    return  {
+      id: formValue.id,
+      version: formValue.version,
+      creationTimestamp: formValue.creationTimestamp,
+      updateTimestamp: formValue.updateTimestamp,
+      creationUser: formValue.creationUser,
+      updateUser: formValue.updateUser,
+      name: formValue.name,
+      description: formValue.description,
+      printerName: formValue.printerName,
+      reportTemplate: formValue.reportTemplate,
+      attrs: {
+        mediaSize: formValue.attrMediaSize,
+        orientation: formValue.attrOrientation,
+        sides: formValue.attrSide,
+        color: formValue.attrColor,
+        copies: formValue.attrCopies
+      }
+    };
+  }
 }
--- frontend/src/app/printer/printer.module.ts
+++ frontend/src/app/printer/printer.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrinterComponent } from './component/printer/printer.component';
 import { PrinterRoutingModule } from './printer-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     PrinterComponent
   ],
-  imports: [
-    CommonModule,
-    PrinterRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        PrinterRoutingModule,
+        ReactiveFormsModule
+    ]
 })
 export class PrinterModule { }
