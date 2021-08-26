b97a016bc7df4fbe2be8213d3f1ed30666c181bb
alberto.rizzi <alberto.rizzi@carel.com>
2021-08-26 18:41:13 +0200

    #35 moved create logic in OrderService

--- backend/src/main/java/be/feastorders/order/service/OrderService.java
+++ backend/src/main/java/be/feastorders/order/service/OrderService.java
@@ -1,14 +1,19 @@
 package be.feastorders.order.service;
 
 import be.feastorders.core.service.BaseCRUDService;
-import be.feastorders.menuitem.dto.MenuItemDTO;
-import be.feastorders.menuitem.entity.MenuItem;
+import be.feastorders.menuitem.service.MenuItemService;
 import be.feastorders.order.dto.OrderDTO;
+import be.feastorders.order.dto.OrderItemDetailDTO;
 import be.feastorders.order.entity.Order;
+import be.feastorders.order.entity.OrderItemDetail;
 import be.feastorders.order.repository.OrderRepository;
+import be.feastorders.printer.entity.PrinterCfg;
+import be.feastorders.printer.service.PrinterAsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Objects;
 
 @Service
@@ -16,13 +21,53 @@ public class OrderService extends BaseCRUDService<Order, Long> {
 
     private final OrderRepository repository;
 
+    @Autowired
+    private MenuItemService menuItemService;
+
+    @Autowired
+    private PrinterAsyncService printerAsyncService;
+
     public OrderService(OrderRepository repository) {
         super(repository);
         this.repository = repository;
     }
 
-    public OrderDTO updateEntityValues(OrderDTO dto) {
+    public OrderDTO createEntity(OrderDTO dto) {
+        Order entity = Order.builder()
+                .client(dto.getClient())
+                .tableNumber(dto.getTableNumber())
+                .placeSettingNumber(dto.getPlaceSettingNumber())
+                .note(dto.getNote())
+                .cashier(dto.getCashier())
+                .takeAway(dto.getTakeAway())
+                .total(dto.getTotal())
+                .orderItemDetails(new ArrayList<>())
+                .build();
 
+
+        for(OrderItemDetailDTO detailDTO: dto.getMenuItemList()){
+            OrderItemDetail detailEntity = new OrderItemDetail();
+
+            detailEntity.setOrder(entity);
+            detailEntity.setQuantity(detailDTO.getQuantity());
+            detailEntity.setTotalPrice(detailDTO.getTotalPrice());
+            detailEntity.setMenuItem(menuItemService.read(detailDTO.getMenuItemId()));
+
+            entity.getOrderItemDetails().add(detailEntity);
+        }
+
+        Order order = super.create(entity);
+
+        if (dto.getPrintOrder()) {
+            // print orchestration post creation, asynchronous
+            Map<PrinterCfg, Order> printerCfgOrderMap = printerAsyncService.splitOrder(order);
+            printerAsyncService.executePrintTasks(printerCfgOrderMap);
+        }
+
+        return new OrderDTO(order);
+    }
+
+    public OrderDTO updateEntityValues(OrderDTO dto) {
         Order entity = super.read(dto.getID());
 
         if (Objects.nonNull(dto.getClient()))
@@ -43,6 +88,16 @@ public class OrderService extends BaseCRUDService<Order, Long> {
         if (Objects.nonNull(dto.getTotal()))
             entity.setTotal(dto.getTotal());
 
-        return new OrderDTO(super.update(entity));
+        // todo update the order item details
+
+        Order order = super.update(entity);
+
+        if (dto.getPrintOrder()) {
+            // print orchestration post creation, asynchronous
+            Map<PrinterCfg, Order> printerCfgOrderMap = printerAsyncService.splitOrder(order);
+            printerAsyncService.executePrintTasks(printerCfgOrderMap);
+        }
+
+        return new OrderDTO(order);
     }
 }
