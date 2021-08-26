7d2d63ae0d1e88672ddeeb3083968c3060d6fec5
alberto.rizzi <alberto.rizzi@carel.com>
2021-08-26 14:05:02 +0200

    #34 add services for retrieve printers and report templates

--- frontend/src/app/printer/component/printer/printer.component.ts
+++ frontend/src/app/printer/component/printer/printer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {PrinterCfgService} from "../../service/printercfg.service";
+import {PrintersService} from "../../service/printers.service";
+import {first} from "rxjs/operators";
+import {PrinterCfg} from "../../interface/printercfg.interface";
+import {PrinterReportTemplateService} from "../../service/printer-report-template.service";
 
 @Component({
   selector: 'printer',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrinterComponent implements OnInit {
 
-  constructor() { }
+  printerCfgList: Array<PrinterCfg> = new Array<PrinterCfg>();
+  printers: Array<string> = new Array<string>();
+  reportTemplates: Array<string> = new Array<string>();
+
+  constructor(private _printerCfgService: PrinterCfgService,
+              private _printersService: PrintersService,
+              private _printerReportTemplateService: PrinterReportTemplateService) { }
 
   ngOnInit(): void {
+    this._loadPrinterCfgList();
+    this._loadPrinterList();
+    this._loadPrinterReportTemplateList();
+  }
+
+  private _loadPrinterCfgList() {
+    this._printerCfgService.getAll().pipe(first()).subscribe((printerCfgList: PrinterCfg[]) => {
+      this.printerCfgList = printerCfgList;
+    });
+  }
+
+  private _loadPrinterList() {
+    this._printersService.getAll().pipe(first()).subscribe(printers => {
+      this.printers = printers;
+    });
+  }
+
+  private _loadPrinterReportTemplateList() {
+    this._printerReportTemplateService.getAll().pipe(first()).subscribe(reportTemplates => {
+      this.reportTemplates = reportTemplates;
+    });
   }
 
 }
--- frontend/src/app/shared/enums/api-resource.enum.ts
+++ frontend/src/app/shared/enums/api-resource.enum.ts
@@ -2,6 +2,7 @@ export enum ApiResourceEnum {
     ORDER = "order",
     CATEGORY = "category",
     MENU_ITEM = "menuitem",
+    PRINTERS = "printer/list",
     PRINTER_CFG = "printer/cfg",
     PRINTER_REPORT_TEMPLATE = "printer/reportTemplate"
 }
