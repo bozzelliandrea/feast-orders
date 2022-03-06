66e88006b1bfab0cdb4e7411fd8d41229ae67e07
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-03-06 14:52:21 +0100

    sostituzione ordini

--- backend/atomic/src/main/java/atomic/repository/MenuItemRepository.java
+++ backend/atomic/src/main/java/atomic/repository/MenuItemRepository.java
@@ -1,10 +1,7 @@
 package atomic.repository;
 
 import atomic.entity.MenuItem;
-import atomic.entity.OrderItemDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,10 +10,4 @@ import java.util.List;
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 
     List<MenuItem> findByCategoryID(Long categoryId);
-
-    @Query("SELECT od " +
-            "FROM OrderItemDetail od " +
-            "JOIN od.order o " +
-            "WHERE o.ID = :orderId")
-    List<OrderItemDetail> findMenuItemsByOrderId(@Param("orderId") Long orderID);
 }
